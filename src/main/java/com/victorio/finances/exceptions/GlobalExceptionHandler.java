5bc6258c906bf41d46d4b35fa31f8e179daeaed9
victorio nogueira <victorionogueira@gmail.com>
2025-03-18 14:04:21 -0300

    Add global exception handler for validation errors

--- src/main/java/com/victorio/finances/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/victorio/finances/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,18 @@
 package com.victorio.finances.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// Importar http, validation, bind, context e servlet
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +24,20 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> HandleUnexpectedException(Exception e) {
 		return ResponseEntity.status(500).body("Unexpected error: " + e.getLocalizedMessage());
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+	            HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		Map<String, String> errors = new HashMap<>();
+	        
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField(); 
+			String message = error.getDefaultMessage(); 
+	        errors.put(fieldName, message); 
+	    	});
+
+	    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
 }
