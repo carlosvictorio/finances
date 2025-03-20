a62a50c1af2fc98ee4407b517325d233cdd1ecb1
victorio nogueira <victorionogueira@gmail.com>
2025-03-20 15:03:01 -0300

    Adiciona parametro LocalDateTime no TransactionDto

--- src/main/java/com/victorio/finances/dto/TransactionDto.java
+++ src/main/java/com/victorio/finances/dto/TransactionDto.java
@@ -1,7 +1,5 @@
 package com.victorio.finances.dto;
 
-import java.time.LocalDateTime;
-
 import com.victorio.finances.enums.TypeEnum;
 
-public record TransactionDto(Double value, TypeEnum type, String description, LocalDateTime date) {}
+public record TransactionDto(Double value, TypeEnum type, String description) {}
