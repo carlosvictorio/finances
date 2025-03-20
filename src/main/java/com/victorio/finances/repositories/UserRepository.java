73d595b0f9bb71878c3c455fbf9d0013e3f2afa7
victorio nogueira <victorionogueira@gmail.com>
2025-03-20 14:58:32 -0300

    removendo import desnecessario

--- src/main/java/com/victorio/finances/repositories/UserRepository.java
+++ src/main/java/com/victorio/finances/repositories/UserRepository.java
@@ -1,10 +1,7 @@
 package com.victorio.finances.repositories;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import com.victorio.finances.models.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, Long>{
